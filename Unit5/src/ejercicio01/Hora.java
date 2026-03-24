e7ce0cdb3c2c6e8ca861ec27ca437c21c3013a07
Rei ヽ( ´￢`)ﾉ <mr.nkwar@iesnervion.es>
2026-03-24 14:40:29 +0100

    holii

---
+++ Unit5/src/ejercicio01/Hora.java
@@ -0,0 +1,27 @@
+package ejercicio01;
+
+public class Hora {
+	private int hora;
+	private int minuto;
+	
+	Hora(int hora, int minuto){
+		this.hora = hora;
+		this.minuto = minuto;
+	}
+
+	public int getHora() {
+		return hora;
+	}
+
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+
+	public int getMinuto() {
+		return minuto;
+	}
+
+	public void setMinuto(int minuto) {
+		this.minuto = minuto;
+	}
+}
